ad492b9621c0d565f81612f5049f1bb9e31dafed
Leslie-L <leslie.centeno@galileo.edu>
2024-05-15 13:38:54 -0600

    collection to add friends

--- src/db/services/Firebase/addFriend.ts
+++ src/db/services/Firebase/addFriend.ts
@@ -1,4 +1,4 @@
-import {collection, addDoc,deleteDoc,doc} from "firebase/firestore"; 
+import {collection, addDoc,deleteDoc,doc, setDoc} from "firebase/firestore"; 
 import { db } from "../../credentials";
 
 
@@ -6,8 +6,12 @@ async function addFriend(u1:string, u2:string,req:string) {
     try {
         const docRef = collection(db, "friends");
         const data ={users:[u1,u2]}
-        await addDoc(docRef, data);
+        const newDoc = await addDoc(docRef, data);
         await deleteDoc(doc(db, "requestfriend", req));
+        const id = newDoc.id;
+        const docRefChat = doc(db,"chats",id);
+        await setDoc(docRefChat,data)
+
         
     } catch (error) {
         throw new Error("Error: ");
